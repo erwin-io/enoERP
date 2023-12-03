2963872cd23421fef8afa61bd96adfeee1387378
erwin-io <erwinramirez220@gmail.com>
2023-12-04 02:03:34 +0800

    environment.ts logic

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://eno-erp-api.vercel.app/api/v1",
+  apiBaseUrl: process.env["ENV"] && process.env["ENV"] === 'PROD' ? "https://eno-erp-api.vercel.app/api/v1" : "https://eno-erp-api-stage.vercel.app/api/v1",
 };
--- web/src/environments/environment.stage.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: "https://eno-erp-api-stage.vercel.app/api/v1",
-};

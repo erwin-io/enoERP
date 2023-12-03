e142aa816cbbb24c9e05046c67c25ce0e3f5b0c2
erwin-io <erwinramirez220@gmail.com>
2023-12-04 03:22:57 +0800

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,15 @@ import * as bodyParser from "body-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      "http://localhost:4200",
+      "https://eno-erp-web-stage.vercel.app",
+      "https://eno-erp-web.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: false,
+  });
   app.setGlobalPrefix("api/v1");
   // the next two lines did the trick
   app.use(bodyParser.json({ limit: "50mb" }));
@@ -29,15 +37,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("swagger", app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
-    customfavIcon: 'https://avatars0.githubusercontent.com/u/7658037?v=3&s=200',
+    customfavIcon: "https://avatars0.githubusercontent.com/u/7658037?v=3&s=200",
     customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
     ],
     customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
     ],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
